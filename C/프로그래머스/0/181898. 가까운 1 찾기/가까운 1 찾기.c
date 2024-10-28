0d363bddd3ea6088b4b006c5aedba0f539323750
Esther <77833937+Esther-Jang@users.noreply.github.com>
2024-10-28 23:04:24 +0900

    Update 가까운 1 찾기.c

--- C/프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.c
+++ C/프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.c
@@ -3,9 +3,7 @@
 #include <stdlib.h>
 
 // arr_len은 배열 arr의 길이입니다.
-int solution(int arr[], size_t arr_len, int idx) {
-    int answer = 0;
-    
+int solution(int arr[], size_t arr_len, int idx) {    
     for(size_t i=idx;i<arr_len;i++){
         if(arr[i]==1){
             return i;
