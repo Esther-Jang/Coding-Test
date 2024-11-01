dd490659be7ae7fee1c9d725491ec02511f020d2
Esther <77833937+Esther-Jang@users.noreply.github.com>
2024-11-01 18:46:28 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ C/프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* myString, const char* pat) {
+    char* answer = (char*)malloc(strlen(myString)+1);
+    
+    int n=-1;
+    for(int i=0;i<strlen(myString);i++){
+        if(strncmp(pat,myString+i,strlen(pat))==0)
+            n=i;
+    }
+    
+    strncpy(answer,myString,n+strlen(pat));
+    answer[n+strlen(pat)]='\0';
+    
+    return answer;
+}
