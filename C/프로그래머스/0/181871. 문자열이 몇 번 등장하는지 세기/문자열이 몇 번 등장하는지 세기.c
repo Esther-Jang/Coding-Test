398d3be50a0be6eaac182161ee0c981ee9e1054e
Esther <77833937+Esther-Jang@users.noreply.github.com>
2024-11-01 18:48:29 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.02 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ C/프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* myString, const char* pat) {
+    int answer = 0;
+    
+    for(int i=0;i<strlen(myString);i++){
+        if(strncmp(myString+i,pat,strlen(pat))==0)
+            answer++;
+    }
+    
+    return answer;
+}
